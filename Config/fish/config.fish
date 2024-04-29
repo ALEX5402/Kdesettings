e6ccb5762f518b3bbdd99c31520de9695eb9a7b6
Alex <alex5402private@gmail.com>
2024-04-29 10:14:19 +0530

    Update Configs Mon Apr 29 10:14:19 AM IST 2024
    Date: Mon Apr 29 10:14:19 AM IST 2024
    Signed-off-by: Alex <alex5402private@gmail.com>

--- Config/fish/config.fish
+++ Config/fish/config.fish
@@ -169,7 +169,7 @@ alias vim 'nvim'
 alias print-fingerprint-jar 'keytool -printcert -jarfile'
 alias wayshere 'sudo mount --bind ~/Androidshere ~/.local/share/waydroid/data/media/0/Documents'
 alias waystart 'bash ~/waydroid.sh'
-alias fish 'neofetch'
+alias fish 'fastfetch'
 
 alias warpc 'warp-cli connect'
 alias warpd 'warp-cli disconnect'
