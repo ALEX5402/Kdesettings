d0dd56c58e1deeb8db45a4526a14f674f73f6270
Alex <alex5402private@gmail.com>
2024-04-29 10:18:10 +0530

    Update Configs Mon Apr 29 10:18:10 AM IST 2024
    Date: Mon Apr 29 10:18:10 AM IST 2024
    Signed-off-by: Alex <alex5402private@gmail.com>

--- Config/fish/config.fish
+++ Config/fish/config.fish
@@ -136,7 +136,7 @@ end
 
 
 # Replace ls with eza
-alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
+alias ls 'eza --color=always --group-directories-first --icons' # preferred listing
 alias la 'eza -a --color=always --group-directories-first --icons'  # all files and dirs
 alias ll 'eza -l --color=always --group-directories-first --icons'  # long format
 alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
@@ -147,7 +147,6 @@ alias cat 'bat --style header --style snip --style changes --style header'
 if not test -x /usr/bin/yay; and test -x /usr/bin/paru
     alias yay 'paru'
 end
-
 
 alias icat="kitten icat"
 
