fed3e0076a71586972cbab72f53e9691dd0ccf19
Alex <alex5402private@gmail.com>
2024-05-01 22:48:42 +0530

    Update Configs : Wed May  1 10:48:42 PM IST 2024
    Time : 22:48:42
    Signed-off-by: Alex <alex5402private@gmail.com>

--- auto-sync.sh
+++ auto-sync.sh
@@ -2,7 +2,7 @@
 
 #set the backup date
 date > time.txt
-git diff . > changes.md  
+git diff . > changes.txt
 git add .
 git commit -m "Update Configs : $(date)" -m "Time : $(date +%T)" -s
 git push -f
