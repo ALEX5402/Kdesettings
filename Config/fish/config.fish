03c27e53f424386f8761428a189a08da709d08d8
Alex <alex5402private@gmail.com>
2024-05-01 11:34:21 +0530

    Update Configs : Wed May  1 11:34:20 AM IST 2024
    Time : 11:34:20
    Signed-off-by: Alex <alex5402private@gmail.com>

--- Config/fish/config.fish
+++ Config/fish/config.fish
@@ -220,7 +220,7 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
 export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
 export FZF_CTRL_T_OPTS="--preview '$show_file_or_dir_preview'"
-export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+
 
 # Add ~/.cargo/bin to PATH
 if test -d ~/.cargo/bin
